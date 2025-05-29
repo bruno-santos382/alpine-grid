66438d9affb6c5bb5ba6f5fc68f6c3cf3531973e
bruno-santos382 <166670969+bruno-santos382@users.noreply.github.com>
2025-05-28 22:10:37 -0300

    Update datagrid.js

--- datagrid.js
+++ datagrid.js
@@ -1,7 +1,17 @@
-class Datagrid {
-    constructor(element) {
-        this.el = element;
-        this.colunas = [];
+// class Datagrid {
+//     constructor(element) {
+//         this.el = element;
+//         this.colunas = [];
+//     }
+
+//     destroy() {
+//         console.log('Destroy');
+//     }
+// }
+
+class Grid {
+    constructor(el) {
+        this.el = el;
     }
 
     destroy() {
@@ -9,15 +19,51 @@ class Datagrid {
     }
 }
 
+export default function(Alpine) {
+    Alpine.directive('grid', (el, { expression, value }, { evaluate, cleanup }) => {
 
+        const diretives = {
+            'each': () => {
+                const rows = [
+                    {
+                        name: 'name',
+                        description: 'description',
+                        price: 'price',
+                        id: 'id'
+                    }
+                ]
 
-export default function (Alpine) {
-    Alpine.directive('grid', (el, { expression, value }, { cleanup }) => {
+                rows.forEach((row, index) => {
+                    const clone = el.content.cloneNode(true)
+                    Alpine.addScopeToNode(clone, { row })
+                    el.after(clone)
+                    console.log(clone)
 
-        if (value == null) {
-            const grid = new Datagrid(el);
-            console.log('initialize grid')
-            cleanup(() => grid.destroy());
+                })
+            },
+            'col': () => {
+                console.log('col', )
+            },
+            'hide': () => {
+                console.log('hide', )
+            },
+            'no-sort': () => {
+                console.log('no-sort', )
+            }
         }
+
+        if (value !== null) {
+            if (value in diretives) {
+                diretives[value]();
+                return;
+            }
+
+            console.error(`Atributo x-grid:${value} é inválido`);
+            return;
+        }
+
+        const grid = new Grid(el);
+        console.log('initialize grid')
+        cleanup(() => grid.destroy());
     });
 }
